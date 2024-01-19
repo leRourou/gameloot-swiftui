71488635dfd9b232a531af815d32fb5530f9035d
Axel ROUQUETTE <rouqueta@d250-m01.iut-acy.local>
2024-01-19 17:25:04 +0100

    Continue working on Loots

--- LootDetailView.swift
+++ LootDetailView.swift
@@ -21,12 +21,33 @@ struct LootDetailView: View {
                     .font(.system(size: 80))
                     .foregroundColor(.white)
             )
-            .padding(.bottom, 20)
+            .padding(.bottom, 50)
         Text(item.name)
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            .font(.system(size: 36))
+            .foregroundStyle(Color(item.rarity.getColor()))
+        if (item.rarity == Rarity.unique) {
+            VStack() {
+                Rectangle()
+                    .fill(item.rarity.getColor())
+                    .cornerRadius(20)
+                    .frame(height: 50)
+                    .padding(10)
+                    .overlay(
+                        Text("Item Unique 🏆")
+                            .font(.system(size: 15))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                    )
+            }
+        }
+        Group {
+            Text("try")
+        }
     }
 }
 
 #Preview {
-    LootDetailView(item: MockData.lootItemsMock[0])
+    LootDetailView(item: MockData.lootItemsMock[10])
 }
 
